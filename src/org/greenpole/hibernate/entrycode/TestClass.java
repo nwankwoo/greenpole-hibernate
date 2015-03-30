6337d1122a683219e05a22020858ab0183aa0a38
akinwaleagbaje <akinwaleagbaje@yahoo.com>
2015-03-30 10:40:46 +0100

    Created packages and added test classes with information on package usage

---
+++ src/org/greenpole/hibernate/entrycode/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.hibernate.entrycode;
+
+/**
+ *
+ * @author Akinwale.Agbaje
+ */
+public class TestClass {
+    //This is just a test class. Classes in this pack will be consist of suggested
+    //codes for the project. Once agreed upon, these codes will be added to the main
+    //project code
+}
